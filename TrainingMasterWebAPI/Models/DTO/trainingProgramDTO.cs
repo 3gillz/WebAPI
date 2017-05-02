937e5ccd805eaa9af7a22c1bce71e4fb8a15432c
dmrgunnarsson <dmrgunnarsson@gmail.com>
2017-05-02 20:31:08 +0000

    DTO's

---
+++ TrainingMasterWebAPI/Models/DTO/trainingProgramDTO.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace TrainingMasterWebAPI.Models.DTO
+{
+    public class trainingProgramDTO
+    {
+      
+              public int TPID { get; set; }
+        public int training_TID { get; set; }
+        public int trainer_TRID { get; set; }
+    
+      
+    }
+}
