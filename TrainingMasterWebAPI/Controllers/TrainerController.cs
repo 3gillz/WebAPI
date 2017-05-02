535d8198d4d5d3dab939bfc214f362ae1652309e
ivargj <ivarj.v16.nemandi.ntv.is>
2017-05-02 21:55:26 +0000

    Added controllers

---
+++ TrainingMasterWebAPI/Controllers/TrainerController.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Web.Http;
+
+namespace TrainingMasterWebAPI.Controllers
+{
+    public class TrainerController : ApiController
+    {
+        
+    }
+}
