cbbcdabbcbb384e1335c22acb9798f55eece4b1d
dmrgunnarsson <dmrgunnarsson@gmail.com>
2017-05-03 02:39:01 +0000

    la ittle fixup

--- TrainingMasterWebAPI/Controllers/ExerciseController.cs
+++ TrainingMasterWebAPI/Controllers/ExerciseController.cs
@@ -4,10 +4,16 @@ using System.Linq;
 using System.Net;
 using System.Net.Http;
 using System.Web.Http;
+using TrainingMasterWebAPI.Queries;
 
 namespace TrainingMasterWebAPI.Controllers
 {
     public class ExerciseController : ApiController
     {
+        readonly private ExerciseQueries cq;
+        public ExerciseController()
+        {
+            cq = new ExerciseQueries();
+        }
     }
 }
--- TrainingMasterWebAPI/Controllers/FoodProgramController.cs
+++ TrainingMasterWebAPI/Controllers/FoodProgramController.cs
@@ -1,15 +1,13 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Linq;
-using System.Net;
-using System.Net.Http;
-using System.Web.Http;
+using System.Web;
+using System.Web.Mvc;
 
 namespace TrainingMasterWebAPI.Controllers
 {
-    public class FoodProgramController : ApiController
+    public class FoodProgramController : Controller
     {
-        
+       
     }
 }
-   
